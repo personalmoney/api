54d612fa1eaaccfad7e083c73feba6209c6e3d2a
Bhagavan Reddy <bhagavan44@gmail.com>
2020-06-07 18:05:40 +0530

    Defined the default route

---
+++ MoneyManager.Api/Controllers/DefaultController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MoneyManager.Api.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class IndexController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult Get()
+        {
+            return Ok("Service active");
+        }
+    }
+}
