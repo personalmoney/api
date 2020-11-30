29d249f1c9e565529158ead54a0ea5ea00368ef9
Bhagavan Reddy <bhagavan44@gmail.com>
2020-11-30 10:18:16 +0530

    Removed the unused columns

--- PersonalMoney.Api/Models/TransactionTag.cs
+++ PersonalMoney.Api/Models/TransactionTag.cs
@@ -6,9 +6,6 @@ namespace PersonalMoney.Api.Models
     [FirestoreData]
     internal class TransactionTag : TimeModel
     {
-        [FirestoreProperty("transactionId")]
-        public string TransactionId { get; set; }
-
         [FirestoreProperty("tagId")]
         public string TagId { get; set; }
     }
