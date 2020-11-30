908e0a60c4487f5c6b74ece2ea62521fed4d4080
Bhagavan Reddy <bhagavan44@gmail.com>
2020-11-30 10:34:31 +0530

    Transaction service initial code

---
+++ PersonalMoney.Api/Helpers/Enums.cs
@@ -0,0 +1,23 @@
+﻿namespace PersonalMoney.Api.Helpers
+{
+    /// <summary>
+    /// Transaction type
+    /// </summary>
+    public enum TransactionType
+    {
+        /// <summary>
+        /// The deposit
+        /// </summary>
+        Deposit = 1,
+
+        /// <summary>
+        /// The withdrawal
+        /// </summary>
+        Withdrawal = 2,
+
+        /// <summary>
+        /// The transfer
+        /// </summary>
+        Transfer = 3
+    }
+}
